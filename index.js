fe3edf0b22a302ea8df430b5bb4f04f346c3bf47
Edgar <muchemiedgar2@gmail.com>
2025-03-10 16:08:41 +0300

    implement array map method

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    // Split the tutorial title into an array of words
+    const words = tutorial.split(' ');
+    
+    // Capitalize the first letter of each word
+    const capitalizedWords = words.map(word => {
+      // Take the first character and capitalize it, then add the rest of the word
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    
+    // Join the words back together with spaces
+    return capitalizedWords.join(' ');
+  });
 }
